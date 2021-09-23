fea24c513c85498aeefb0b6545cdaabecf2d3e8a
sajjadmirzaei <sajjadmirzaeizahan@gamil.com>
2021-09-23 17:00:10 +0330

    add payment methods part 2

--- app/Http/Controllers/Api/V1/User/PaymentController.php
+++ app/Http/Controllers/Api/V1/User/PaymentController.php
@@ -4,10 +4,87 @@ namespace App\Http\Controllers\Api\V1\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Psr\Http\Message\ResponseInterface;
+
 
 class PaymentController extends Controller
 {
+    private function header($sandbox)
+    {
+        return [
+            'Content-Type: application/json',
+            'X-API-KEY:'.env('IDPAY_API_KEY'),
+            'X-SANDBOX:'.$sandbox
+        ];
+    }
+
+    private function requestHttp($params, $header,$url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1.1/'.$url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+        $result = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return ['result'=>$result,'http_code'=>$httpcode];
+    }
+
+    public $msg;
+
+    public function status_response($status)
+    {
+        switch ($status) {
+            case 1:
+                $this->msg = 'پرداخت انجام نشده است.  ';
+                break;
+            case 2:
+                $this->msg = 'پرداخت ناموفق بوده است.';
+                break;
+            case 3:
+                $this->msg = 'خطا رخ داده است.';
+                break;
+            case 4:
+                $this->msg = 'بلوکه شده.';
+                break;
+            case 5:
+                $this->msg = 'برگشت به پرداخت کننده.';
+                break;
+            case 6:
+                $this->msg = 'برگشت خورده سیستمی.';
+                break;
+            case 7:
+                $this->msg = 'انصراف از پرداخت.';
+                break;
+            case 8:
+                $this->msg = 'به درگاه پرداخت منتقل شد.';
+                break;
+            case 10:
+                $this->msg = 'در انتظار تایید پرداخت.';
+                break;
+            case 100:
+                $this->msg = 'پرداخت تایید شده است.';
+                break;
+            case 101:
+                $this->msg = 'پرداخت قبلا تایید شده است.';
+                break;
+
+            case 200:
+                $this->msg = 'به دریافت کننده واریز شد.';
+                break;
+            case 405:
+                $this->msg = 'تایید پرداخت امکان پذیر نیست.';
+                break;
+        }
+    }
+
     protected function sendToGate(Order $order){
         $params = array(
             'order_id' => $order->id,
@@ -16,66 +93,15 @@ class PaymentController extends Controller
             'mail' => auth()->user()->email,
             'callback' => 'https://localhost:8000/api/user/payment/callback',
         );
+        $header=$this->header(1);
+        $response=$this->requestHttp($params,$header,'payment');
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1.1/payment');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'X-API-KEY: 45e66d8e-6726-4455-badf-ed6fa6930e07',
-            'X-SANDBOX: 1'
-        ));
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $this->respond($result);
+        return $this->respond($response);
     }
 
     public function callback(Request $request){
-//        return $request->status;
 
-        switch (request()->status) {
-            case 1:
-                return $this->respondWithMessage('پرداخت انجام نشده است');
-                break;
-            case 2:
-                return $this->respondWithMessage('پرداخت ناموفق بوده است');
-                break;
-            case 3:
-                return $this->respondWithMessage('خطا رخ داده است');
-                break;
-            case 4:
-                return $this->respondWithMessage('بلوکه شده');
-                break;
-            case 5:
-                return $this->respondWithMessage('برگشت به پرداخت کننده');
-                break;
-            case 6:
-                return $this->respondWithMessage('برگشت خورده سیستمی');
-                break;
-            case 7:
-                return $this->respondWithMessage('انصراف از پرداخت');
-                break;
-            case 8:
-                return $this->respondWithMessage('به درگاه پرداخت منتقل شد');
-                break;
-            case 10:
-                return $this->respondWithMessage('در انتظار تایید پرداخت');
-                break;
-            case 100:
-                return $this->respondWithMessage('پرداخت تایید شده است');
-                break;
-            case 101:
-                return $this->respondWithMessage('پرداخت قبلا تایید شده است');
-                break;
-//            case 200:
-//                return $this->respondWithMessage('به دریافت کننده واریز شد');
-//                break;
-//            default:
-//                echo "Your favorite color is neither red, blue, nor green!";
-        }
+        $this->status_response($request->status);
 
         if ($request->status==200){
             $params = array(
@@ -83,24 +109,12 @@ class PaymentController extends Controller
                 'order_id' => $request->order_id,
             );
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1.1/payment/inquiry');
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'X-API-KEY: 45e66d8e-6726-4455-badf-ed6fa6930e07',
-                'X-SANDBOX: 1',
-            ));
+            $header=$this->header(1);
+            $response=$this->requestHttp($params,$header,'inquiry');
 
-            $result = curl_exec($ch);
-            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-
-            var_dump($httpcode);
-            var_dump($result);
+            return $this->respond($response);
         }
-
+        return $this->msg;
     }
 
 }
